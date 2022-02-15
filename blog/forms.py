3e5a6df6aa6e99b5dc284aa81ad18486dc40db79
SalihTpc <salihtopcu@hotmail.com.tr>
2022-02-16 02:31:24 +0300

    some changes

--- blog/forms.py
+++ blog/forms.py
@@ -3,10 +3,7 @@ from .models import Post, Category
 
 choices = Category.objects.all().values_list("name", "name")
 
-choice_list = []
-
-for item in choices:
-    choice_list.append(item)
+choice_list = [choice for choice in choices]
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -27,6 +24,7 @@ class UpdateForm(forms.ModelForm):
         fields = ("title", "category", "body", "image")
         widgets = {
             "title": forms.TextInput(attrs={"class":"form-control"}),
+            "category": forms.Select(choices=choice_list, attrs={"class":"form-control"}),
             "body": forms.Textarea(attrs={"class":"form-control"}),
             "image": forms.TextInput(attrs={"class":"form-control" , "placeholder":"Post picture url should be add"}),
         }
